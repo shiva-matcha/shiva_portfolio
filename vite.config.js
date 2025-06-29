842d47618407305b9e75391d585e53ae2e009e79
Shiva <shivamacha24@gmail.com>
2025-06-30 00:26:00 +0530

    Fix: Clean install dependencies, fix lucide-react issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/your-repo-name/', // Replace with your GitHub repo name
+  base: '/shiva_portfolio/', // Replace with your GitHub repo name
   plugins: [react()]
 })
